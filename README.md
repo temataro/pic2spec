411cc4e423bb1cfa9b4613a5ec8b09f4a943ba8c
Temesgen Ataro <atarotemesgen@gmail.com>
2024-08-12 22:46:20 +0400

    Ready. Set. Go

---
+++ README.md
@@ -0,0 +1,35 @@
+# Pic2Spec
+
+![Pic](./images/Lena.png) 
+![Spec](./images/v0.1.png) 
+
+Convert a picture's intensity into a Fourier transform every x miliseconds,
+iFFT and save to wav.
+
+First convert picture to greyscale.
+Each row of pixels needs to turn into an intensity plot of the same resolution.
+(How would I expand this to other (higher) resolutions?). In FFT, just
+interpolate, for pictures... ML upscale? [TODO]
+
+Convert each row of size `WIDTH` into a time series of N samples where
+N = 2 ** (np.log2(WIDTH) + 1)
+(i.e. a 1920 px row would turn into a 2 ** 12 (=4096) sample sound.)
+Why? Better for FFTs.
+
+Map intensity of pixel to an amplitude.
+Repeat for `HEIGHT` rows.
+
+## Audibility:
+
+  We're free to play with the sample rate we save the wav file at.
+  Typically, 1920x1080 => 4096 * 1080 = 4_423_680 samples
+
+  At 44.1kHz, that's ~100.3 seconds. Too long. Let's do 441kHz. Wait what?
+  OK unreasonable. Let's limit ourselves to audible sound.
+  This means limiting the samp rate to 44.1KHz
+  (our max freq would be 22.05kHz, audible.)
+
+
+TODOs:
+  - Information lost during complex ifft -> real wav file conversion. I'm currently
+  doing 2 channel data (Re/Im)...
---
+++ pic2spec.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import scipy
+import numpy as np
+import numpy.typing as npt
+import matplotlib.image
+import matplotlib.pyplot as plt
+
+
+# TODO: CLI args
+filename: str = "./images/Lena.png"
+
+
+def read_img(filename: str) -> npt.NDArray[np.uint8]:
+    img: npt.NDArray[np.uint8] = matplotlib.image.imread(filename)
+
+    if img.shape[2] == 3:
+        # Convert to greyscale with these magic numbers
+        img = img.dot([0.07, 0.72, 0.21])
+        return img
+
+    if img.shape[2] == 4:
+        # Convert to greyscale with these magic numbers
+        img = img.dot([0.07, 0.72, 0.21, 0])
+        return img
+
+    plt.imshow(img, cmap='grey')
+    plt.show()
+
+    return img
+
+
+def upscale(row: npt.NDArray[np.uint8]) -> npt.NDArray[np.float32]:
+    "Upscale non power of 2 rows to the nearest power of 2."
+
+    width: int = row.size
+    if width == 2 ** np.floor(np.log2(width)):
+        return np.roll(row, width // 2)
+
+    next_pow: int = int(2 ** (np.ceil(np.log2(width))))
+    till_next_pow: int = next_pow - width
+
+    # Rolling makes sure our image's not going to start at 0, but rather at
+    # -next_pow // 2  
+
+    # pad before and after to fill the space
+    return np.roll(np.pad(
+        row, (till_next_pow // 2, till_next_pow // 2 + till_next_pow % 2)
+    ).astype(np.float32), next_pow // 2)
+
+
+def img_to_intensity(img: npt.NDArray[np.uint8]) -> npt.NDArray[np.float32]:
+    width: int = img.shape[0]
+    height: int = img.shape[1]
+
+    intensity: npt.NDArray[np.float32] = np.array(
+        [np.fft.ifft(upscale(img[row])) for row in range(width-1, 0, -1)]
+    ).flatten()
+
+    plt.specgram(intensity, cmap='grey')
+    plt.show()
+
+    return np.vstack(((intensity.real - np.mean(intensity.real)).astype(np.float32), (intensity.imag - np.mean(intensity.imag)).astype(np.float32))).T
+
+
+def intensity_to_wav(audio: npt.NDArray[np.float32], samp_rate: int) -> None:
+    outfile: str = f"./audio/{filename.replace("/", "").split(".")[-2]}.wav"
+        
+    scipy.io.wavfile.write(outfile, samp_rate, audio)
+
+
+def main() -> None:
+    img: npt.NDArray[np.uint8] = read_img(filename)
+    audio: npt.NDArray[np.float32] = img_to_intensity(img)
+    sr: int = int(44.1e3)
+
+    intensity_to_wav(audio, sr)
+
+
+if __name__ == "__main__":
+    main()
